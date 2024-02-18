eb1390ce00eae7431675d4b6ec5ea22ad64bce18
mihosri <113211399+mihosri@users.noreply.github.com>
2024-02-18 14:20:25 -0700

    code cleanup

--- backend/index.js
+++
@@ -1,4 +0,0 @@
-import fetchFlowerData from './apiController';
-
-const wikiData = await fetchFlowerData('List of plants with symbolism');
-console.log(wikiData);
--- petalsandprose/src/components/Header.jsx
+++
@@ -1,3 +0,0 @@
-{/* 
-This is Header component comprising the title and logo of the website
-*/}
