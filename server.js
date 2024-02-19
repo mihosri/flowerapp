9b26bb0d09cea6bdfa9858bc3820f0b01192463b
mihosri <113211399+mihosri@users.noreply.github.com>
2024-02-19 05:14:32 -0700

    Creation of index,html
    setting default route to serve public folder in server.js
    Added font link in index.html
    adding style to index.html

--- server.js
+++ server.js
@@ -3,39 +3,49 @@ const app = express();
 
 const PORT = 1337;
 
-const cors = require('cors')
-const Flower = require('./config')
+//middleware
+
+//serves public folder for the default route
+app.use(express.static('public'))
 
 
-app.use(express.json())
-app.use(cors())
 
-app.get('/', async (req, res) => {
-  const snapshot = await Flower.get()
-  const list = snapshot.docs.map((doc) =>({id: doc.id, ...doc.data()}) )
-  res.send(list)
-})
+//routes
 
 
-app.post('/create', async (req, res) => {
-  const data = req.body
-  await Flower.add(data)
-  res.send({ msg: 'Flower added' })
-})
+// const cors = require('cors')
+// const Flower = require('./config')
 
 
-app.post('/update', async (req, res) => {
-  const id = req.body.id
-  delete req.body.id;
-  const data = req.body;
-  await Flower.doc(id).update(data)
-  res.send({ msg: 'Flower updated' })
-})
+// app.use(express.json())
+// app.use(cors())
 
-app.delete('/delete', async (req, res) => {
-  const id = req.body.id
-  await Flower.doc(id).delete()
-  res.send({ msg: 'Flower deleted' })
-})
+// app.get('/', async (req, res) => {
+//   const snapshot = await Flower.get()
+//   const list = snapshot.docs.map((doc) =>({id: doc.id, ...doc.data()}) )
+//   res.send(list)
+// })
+
+
+// app.post('/create', async (req, res) => {
+//   const data = req.body
+//   await Flower.add(data)
+//   res.send({ msg: 'Flower added' })
+// })
+
+
+// app.post('/update', async (req, res) => {
+//   const id = req.body.id
+//   delete req.body.id;
+//   const data = req.body;
+//   await Flower.doc(id).update(data)
+//   res.send({ msg: 'Flower updated' })
+// })
+
+// app.delete('/delete', async (req, res) => {
+//   const id = req.body.id
+//   await Flower.doc(id).delete()
+//   res.send({ msg: 'Flower deleted' })
+// })
 
 app.listen(PORT, () => console.log(`Server is up and running ${PORT}`))
